a85a96e24eaaa315e949ff4859a4bce74ce6e697
jaspreetkaur03 <jaspreetkaur03@nagarro.com>
2023-06-04 15:39:35 +0530

    first commit

---
+++ src/main/java/com/sample/demo/controller/DemoController.java
@@ -0,0 +1,13 @@
+package com.sample.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    
+	@GetMapping
+	public String message() {
+		return "SpringBoot Application working....";
+	}
+}
